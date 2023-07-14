0655c33a891b639f3c3ac33d461cf44bb01b2642
lachezar-dimitrov <lachezar.dimitrov@atscale.com>
2023-07-14 13:11:03 +0300

    Ignore .next folder

--- .next/static/chunks/pages/sitemap.xml-dce480d0f0f7e03c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[164],{917:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/sitemap.xml",function(){return _(3067)}])},3067:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSP:function(){return t}});var t=!0;u.default=function(){}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=917)}),_N_E=n.O()}]);
